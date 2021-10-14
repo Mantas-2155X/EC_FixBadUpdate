87acc9825775083ed5308f6a06068a689752767b
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2021-10-14 23:26:37 +0300

    initial

---
+++ EC_FixBadUpdate/EC_FixBadUpdate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using HEdit;
+
+namespace EC_FixBadUpdate
+{
+	[BepInProcess("EmotionCreators")]
+	[BepInPlugin(nameof(EC_FixBadUpdate), nameof(EC_FixBadUpdate), VERSION)]
+	public class EC_FixBadUpdate : BaseUnityPlugin
+	{
+		public const string VERSION = "1.0.0";
+
+		private new static ManualLogSource Logger;
+
+		private void Awake()
+		{
+			Logger = base.Logger;
+			
+			Harmony.CreateAndPatchAll(typeof(EC_FixBadUpdate));
+		}
+		
+		[HarmonyTranspiler, HarmonyPatch(typeof(HEditGlobal), "CreateChara")]
+		public static IEnumerable<CodeInstruction> HEditGlobal_CreateChara_ForceZeroId(IEnumerable<CodeInstruction> instructions)
+		{
+			var il = instructions.ToList();
+            
+			var index = il.FindIndex(instruction => instruction.opcode == OpCodes.Ldfld && (instruction.operand as FieldInfo)?.Name == "objCharaParentNull");
+			if (index <= 0)
+			{
+				Logger.Log(LogLevel.Message | LogLevel.Error, "Failed transpiling HEditGlobal_CreateChara_ForceZeroId Ldfld index not found!");
+				return il;
+			}
+    
+			il[index + 1] = new CodeInstruction(OpCodes.Ldc_I4_0);
+
+			return il;
+		}
+	}
+}
---
+++ EC_FixBadUpdate/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EC_FixBadUpdate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
+[assembly: AssemblyProduct("EC_FixBadUpdate")]
+[assembly: AssemblyCopyright("Mantas-2155X aka 2155X Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("DB24C52E-5D80-4CD3-85AD-1CE1AE15E470")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(EC_FixBadUpdate.EC_FixBadUpdate.VERSION)]
+[assembly: AssemblyFileVersion(EC_FixBadUpdate.EC_FixBadUpdate.VERSION)]
